789ff5ee2fc0eb984048e2d20eb5b1c7f13466cd
Xavient <xavient@XAV-102000575local.local>
2019-09-24 13:30:06 +0530

    v1

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,46 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import Example from '../../src'
+import parseText from '../../src';
+
+const Mention = ({ children, id, name }) => {
+  return (
+    <span
+      name={name}
+      id={id}
+      style={{ border: '1px solid #ccc', padding: '0 10px' }}
+    >
+      {children}
+    </span>
+  );
+};
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-html-replace Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        {parseText(
+          `<italic>This is <bold> xml string</bold> with custom nexted markup,<bold> we can get inner markup & attribute  through props.</bold></italic> <mention id ="123" name ="raodurgesh">  this is mention tag with id & name attribute </mention> `,
+          (tag, props) => {
+            if (tag === 'bold') {
+              return <b />;
+            }
+            if (tag === 'italic') {
+              return <i />;
+            }
+            if (tag === 'mention') {
+              const { name, id, innertext } = props;
+              return (
+                <Mention name={name} id={id}>
+                  {innertext}
+                </Mention>
+              );
+            }
+          }
+        )}
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,94 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+const extractPropsFromString = attr => {
+  let propsStr = attr.match(/\s*([\w]*)\s*=\s*"([\w]*)"/gi) || [];
+  return Object.assign(
+    {},
+    ...propsStr
+      .map(s => {
+        let arr = s.split('=');
+        return {
+          [(arr[0] || '').trim()]: (arr[1] || '').replace(/\s*["]+/g, '')
+        };
+      })
+      .flat()
+  );
+};
+
+/**
+ * 
+ * const fn = (tag, props) => {
+    if (tag === 'mention') {
+      return <MentionBlock />;
+    }
+    if (tag === 'bold') {
+      return <b />;
+    }
+    if (tag === 'italic') {
+      return <i />;
+    }
   }
-}
+ */
+
+const parseText = (text = '', fn = () => {}) => {
+  let tags = [];
+  let jsxElements = [];
+  let prevIndex = -1;
+  const matches = text.matchAll(/<\s*(\/\s*)*([^>\s]*)[^><]*>/gi);
+
+  for (let match of matches) {
+    let [substring, isClosing, tag] = match;
+    let index = match['index'];
+    tag = (tag || '').trim();
+    isClosing = (isClosing || '').trim();
+    substring = (substring || '').trim();
+    if (!isClosing) {
+      if (tags.length === 0) {
+        if (index > prevIndex) {
+          jsxElements.push(text.slice(prevIndex + 1, index));
+        }
+      }
+      tags.push({ tag, substring, index });
+    } else if (
+      isClosing &&
+      tags.length > 0 &&
+      tag === tags[tags.length - 1].tag
+    ) {
+      let tagObj = tags.pop();
+      if (tags.length === 0) {
+        const { substring, tag } = tagObj;
+        let attr = substring.replace(/[<>]/gi, '').replace(tag, '');
+        let props = extractPropsFromString(attr);
+        const innertext = text.slice(
+          text.indexOf('>', tagObj.index) + 1,
+          index
+        );
+        const JsxEl = fn(tag, { ...props, innertext });
+
+        let el = null;
+        if (JsxEl) {
+          el = React.cloneElement(JsxEl, {}, parseText(innertext, fn));
+        } else {
+          el = `${substring} innertext </${tag}>`;
+        }
+        jsxElements.push(el);
+        prevIndex = text.indexOf('>', index);
+      }
+    } else {
+      throw Error('Invalid string, no matching tag "' + tag + '"');
+    }
+  }
+  if (tags.length !== 0) {
+    throw Error(
+      'Invalid string, no matching tags : "' +
+        (tags.map(t => t.tag) || '').toString() +
+        '"'
+    );
+  }
+  if (text.length - 1 > prevIndex) {
+    jsxElements.push(text.slice(prevIndex + 1));
+  }
+  return jsxElements;
+};
+
+export default parseText;
